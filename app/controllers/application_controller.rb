0169fc02cc8ac8e52e58803ff99bd20c3d167930
siroyuri <siroyuri_1110@yahoo.co.jp>
2021-11-24 20:43:39 +0900

    新規登録、ログインの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+   before_action :configure_parmitted_parameters, if: :devise_controller?
+
+   def configure_parmitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+   end
 end
