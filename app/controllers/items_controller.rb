0d296b92a09942ba2304a8727ec15ceaf3662ed3
siroyuri <siroyuri_1110@yahoo.co.jp>
2021-12-04 23:12:55 +0900

    編集情報の保存処理の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,13 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      #item = Item.find(params[:id])
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
