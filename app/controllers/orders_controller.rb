18c9462740fc8d0a028c9591804552c87bb05d87
siroyuri <siroyuri_1110@yahoo.co.jp>
2021-12-08 21:42:22 +0900

    Formオブジェクトを使用して値を保存できるように記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,27 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.new
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      #binding.pry
+      @order_address.save
+      redirect_to root_path
+    else
+      #binding.pry
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(
+      :postcode, :prefecture_id, :city, :house_number, :building_name, :phone_number
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
