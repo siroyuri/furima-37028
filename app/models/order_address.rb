d21bc827f5e8a2a1f388db0969e01c565b504de1
siroyuri <siroyuri_1110@yahoo.co.jp>
2021-12-09 18:52:09 +0900

    エラーメッセージに半角じゃ無いとダメだと明記

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,10 +4,10 @@ class OrderAddress
 
   with_options presence: true do
     validates :token, :user_id, :item_id, :city, :house_number
-    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Input example(123-4567)" }
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is enter half-width characters as in the example(123-4567)" }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: "is too short" }
-    validates :phone_number, numericality: { only_integer: true , message: "is invalid. Input example(09012345678)" }
+    validates :phone_number, numericality: { only_integer: true , message: "is enter half-width numbers as in the example(00012345678)" }
   end
   
   def save
