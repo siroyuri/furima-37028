58f514d446a563c4a0628e635f256bde82d261e8
siroyuri <siroyuri_1110@yahoo.co.jp>
2021-12-09 17:24:43 +0900

    決済処理の記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,13 +6,12 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
     if @order_address.valid?
-      #binding.pry
+      order_payjp
       @order_address.save
       redirect_to root_path
     else
-      #binding.pry
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
@@ -22,6 +21,15 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(
       :postcode, :prefecture_id, :city, :house_number, :building_name, :phone_number
-    ).merge(user_id: current_user.id, item_id: params[:item_id])
+    ).merge( user_id: current_user.id, item_id: params[:item_id], token: params[:token] )
+  end
+
+  def order_payjp
+    Payjp.api_key = ENV["FURIMA_PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
---
+++ config/initializers/webpaker.rb
@@ -0,0 +1,1 @@
+Webpacker::Compiler.env["FURIMA_PAYJP_PUBLIC_KEY"] = ENV["FURIMA_PAYJP_PUBLIC_KEY"]
